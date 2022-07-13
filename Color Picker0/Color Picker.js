bba412ab8c859b1ece01eb55e1dceec7eef9dcb2
RAKSHITHANANDA P <rakshithanandap@gmail.com>
2022-07-13 20:52:15 +0530

    Practice

---
+++ Color Picker0/Color Picker.js
@@ -0,0 +1,29 @@
+let containerText = document.getElementById("selectedColorHexCode")
+
+function button1() {
+    let colorText = document.getElementById("button1").textContent
+    document.getElementById("colorPickerContainer").style.backgroundColor = colorText
+    containerText.textContent = colorText
+
+}
+
+function button2() {
+    let colorText = document.getElementById("button2").textContent
+    document.getElementById("colorPickerContainer").style.backgroundColor = colorText
+    containerText.textContent = colorText
+
+}
+
+function button3() {
+    let colorText = document.getElementById("button3").textContent
+    document.getElementById("colorPickerContainer").style.backgroundColor = colorText
+    containerText.textContent = colorText
+
+}
+
+function button4() {
+    let colorText = document.getElementById("button4").textContent
+    document.getElementById("colorPickerContainer").style.backgroundColor = colorText
+    containerText.textContent = colorText
+
+}
---
+++ Seasons Switcher/Seasons Switcher.js
@@ -0,0 +1,26 @@
+let smallImage = document.getElementById("seasonSmallImage");
+let mediumImage = document.getElementById("seasonMediumImage");
+
+function springBtn() {
+    smallImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png"
+    mediumImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png"
+
+}
+
+function summerBtn() {
+    smallImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png"
+    mediumImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png"
+
+}
+
+function autumnBtn() {
+    smallImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png"
+    mediumImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png"
+
+}
+
+function winterBtn() {
+    smallImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png"
+    mediumImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png"
+
+}
